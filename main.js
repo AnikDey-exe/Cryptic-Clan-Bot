61722b92bff2c08a822f94deb761bf102234350c
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-01-17 14:51:46 -0500

    changed code

--- main.js
+++ main.js
@@ -7,4 +7,19 @@ bot.on("ready", async () => {
     console.log('Cryptic Clan bot is online!');
 })
 
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    
+
+    if(cmd === `${prefix}command goes here`){
+        return message.reply("reply goes here")
+    }
+
+})
+
 bot.login(process.env.token);
