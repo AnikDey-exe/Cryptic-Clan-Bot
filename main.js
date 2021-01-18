c214a52207978776b8550f3950e9700257ade8d2
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-01-17 19:39:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -90,8 +90,8 @@ bot.on("message", async message => {
 })
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+    min = Math.ceil(1);
+    max = Math.floor(2);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
