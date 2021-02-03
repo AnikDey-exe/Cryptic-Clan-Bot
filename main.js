61af5ee0a4d7e22be362923bda35835908733101
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-02-03 14:05:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ let censor = "[Sorry, I Swear]";
 
 bot.on('message', message => {
     if(message.content.toLowerCase().includes("asshole")){
+        message.channel.type === (`"dm"`) + message.author.sendMessage(`"Do not say that at all!"`);
         message.delete();
         message.reply("No NSFW words!");
-        msg.channel.type === (`"dm"`) + msg.author.sendMessage(`"Do not say that at all!"`);
     }
 
     if(message.content.toLowerCase().includes("kkk")){
