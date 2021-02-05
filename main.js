b9f92663ca930004a766542f85cf940c12d1d283
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-02-04 19:54:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ bot.on('message', message => {
     let memberRole = member.guild.roles.find("name", "Member");
     let staffRole = member.guild.roles.find("name", "Board of Directors");*/
 
-    if(message.content.toLowerCase().includes("everyone") &&   
+   /* if(message.content.toLowerCase().includes("everyone") &&   
     !message.member.roles.cache.some(role => role.name === "Administrator") || 
     !message.member.roles.cache.some(role => role.name === "Moderator")) {
         //message.channel.type === (`"dm"`) + message.author.send(`Do not mass mention!`);
@@ -63,7 +63,7 @@ bot.on('message', message => {
               text: "© Cryptic Clan Moderation"
             }
         }});
-    }
+    }*/
 
     if(message.content.toLowerCase().includes("asshole")){
         message.channel.type === (`"dm"`) + message.author.send(`Do not say that phrase at all!`);
@@ -131,7 +131,7 @@ bot.on('message', message => {
               text: "© Cryptic Clan Moderation"
             }
         }});
-        message.author.kick();
+        message.member.kick("Racial slur.");
     }
 
     if(message.content.toLowerCase().includes("dick")){
