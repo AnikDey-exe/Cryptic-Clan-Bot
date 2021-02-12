d02fa0f907dd896aed14f517b7c540701069b508
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-02-11 19:31:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ let censor = "[Sorry, I Swear]";
 })*/
 
 bot.on('message', message => {
-    let mutedRole= message.member.guild.roles.cache.find(role => role.name === "Muted");
+    let mutedRole= message.member.roles.cache.find(role => role.name === "Muted");
     //let member = message.member;
     /*let adminRole = member.guild.roles.find("name", "Administrator");
     let modRole = member.guild.roles.find("name", "Moderator");
