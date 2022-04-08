72678d3c167b3d04d06ae551230ca78bed27fe49
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2022-04-08 13:18:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -269,7 +269,7 @@ bot.on("message", async message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let command = args.shift().toLowerCase();
 
-    if(command === `${prefix}`+'args') {
+    if(command === 'args') {
         if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author.username}!`);
 		}
